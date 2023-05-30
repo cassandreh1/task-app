fda9209b3e32020f2c4358695b52fa014d22b68a
cassandreh1 <cassandreh01@gmail.com>
2023-05-30 14:07:15 -0400

    Adding application components, http & font awesome modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
+  // for components
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonsComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
+  // for modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
